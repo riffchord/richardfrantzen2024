712d91f76e82d6a71feb93fad9772b1bf5ed4b47
riffchord <33197481+riffchord@users.noreply.github.com>
2023-12-24 03:11:56 +0100

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,16 +1,1 @@
 // sanity.config.ts
-import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-
-export default defineConfig({
-  name: "richardfrantzen",
-  title: "Richardfrantzen.se",
-  projectId: "yxcxwx9i",
-  dataset: "production",
-  plugins: [deskTool()],
-  schema: {
-    types: [
-      /* your content types here*/
-    ],
-  },
-});
