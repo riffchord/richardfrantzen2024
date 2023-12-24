2959eee9c996b1c767b57b3617fb5dd0b1dc61fc
riffchord <33197481+riffchord@users.noreply.github.com>
2023-12-24 03:10:13 +0100

    Update api.js

--- src/sanity/api.js
+++
@@ -1,8 +0,0 @@
-import { useSanityClient } from "astro-sanity"
-
-export async function getAllPosts() {
-    const client = useSanityClient()
-    const query = '*[_type == "post"]'
-    const posts = await client.fetch(query)
-    return posts;
-}
